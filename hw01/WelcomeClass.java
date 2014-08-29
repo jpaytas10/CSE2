b3460e979bffe90d014eafb8af2db31747c75729
Jake Paytas <jlp218@lehigh.edu>
2014-08-29 14:10:44 +0000

    updated WelcomeClass

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,31 @@
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Jake Paytas
+//Homework 01
+//'Welcome' Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+//  then, run the program
+//      java WelcomeClass//
+
+//  define class
+public class WelcomeClass{
+    
+//  add main method
+  public static void main(String[] args) {
+      
+//  print out the statement with decorations around it
+    System.out.println(" -----------");
+    System.out.println(" | WELCOME |");
+    System.out.println(" -----------");
+    System.out.println("  ^   ^   ^   ^   ^   ^");
+    System.out.println(" / \\ / \\ / \\ / \\ / \\ / \\");
+    System.out.println("  |-J--L--P--2--1--8-|");
+    System.out.println(" \\ / \\ / \\ / \\ / \\ / \\ /" );
+    System.out.println("  v   v   v   v   v   v");
+    System.out.println("Last year I spent about five months designing, building, and wiring an electronic");
+    System.out.println("'laser harp.' An arduino-based MIDI controller, play ANY sound breaking laserbeams!");
+    
+    //test/testtest
+  }
+}
